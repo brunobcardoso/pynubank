8b56bd9eff6a1d672c0348fb44b39b7eae1c6724
André Roggeri Campos <a.roggeri.c@gmail.com>
2019-04-02 13:28:11 -0300

    fix: Jupyter support for QRCode auth (Closes #36)

---
+++ pynubank/utils.py
@@ -0,0 +1,10 @@
+from qrcode import QRCode
+
+def print_qr_code(uuid: str):
+    qr = QRCode()
+    qr.add_data(uuid)
+    try:
+        get_ipython
+        qr.make_image(fill_color="#111", back_color="#ccc")
+    except NameError:
+        qr.print_ascii(invert=True)
